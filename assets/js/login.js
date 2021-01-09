e732093af98c2e5ff6633272e86292358acb1e35
chenxi <1172072261@qq.com>
2021-01-09 10:29:22 +0800

    建立本地仓库

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function () {
+    // 点击注册账号的链接
+    $('#link-reg').on('click', function () {
+        
+    })
+    // 点击去登录的链接
+    $('#link-login').on('click', function () {
+        
+    })
+})
